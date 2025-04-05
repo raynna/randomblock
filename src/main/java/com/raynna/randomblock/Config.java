90d5f706098aaf1486b32a4373dcf996de3ba4f3
Raynna <andreas.henriksson96@gmail.com>
2025-04-05 04:08:47 +0200

    Cleaned up ui settings config

--- src/main/java/com/raynna/randomblock/Config.java
+++ src/main/java/com/raynna/randomblock/Config.java
@@ -57,7 +57,7 @@ public class Config {
 
         public static final ModConfigSpec.IntValue CLIENT_CONFIG_VERSION;
 
-        public static ModConfigSpec.EnumValue<Client.GuiMode> SHOW_GUI;
+        public static ModConfigSpec.EnumValue<GuiMode> SHOW_GUI;
         public enum GuiMode { SHOW, HIDE }
 
         public enum GuiPosition {
@@ -80,15 +80,14 @@ public class Config {
             ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
             CLIENT_CONFIG_VERSION = builder.translation("Client Config Version: ").comment("DO NOT CHANGE. Used for tracking config updates.").defineInRange("config_version", CLIENT_VERSION, 1, Integer.MAX_VALUE);
             SHOW_GUI = builder.translation("Show Overlay: ")
-                    .comment("Determine if you want to see timers for next block and item spawns, Valid values are: SHOW, HIDE")
+                    .comment("Determine which timers should be shown or hidden")
                     .comment("Default: SHOW")
                     .defineEnum("show_gui", Client.GuiMode.SHOW);
-            GUI_POSITION = builder.translation("Overlay Position: ")
-                    .comment("Where should the timer UI appear on the screen?")
-                    .comment("Options: TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT, CENTER")
+            GUI_POSITION = builder.translation("Timer Overlay Position: ")
+                    .comment("Location for timer overlay")
                     .defineEnum("gui_position", GuiPosition.BOTTOM_CENTER);
-            X_PADDING = builder.translation("Overlay X Adjustments: ").comment("Fine adjust the X position for the overlay").comment("Default: 10").defineInRange("x_adjustment", 10, Integer.MIN_VALUE, Integer.MAX_VALUE);
-            Y_PADDING = builder.translation("Overlay Y Adjustments: ").comment("Fine adjust the Y position for the overlay").comment("Default: 10").defineInRange("y_adjustment", 10, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            X_PADDING = builder.translation("Overlay X Adjustments: ").comment("Fine adjust the X position for the overlay").defineInRange("x_adjustment", 10, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            Y_PADDING = builder.translation("Overlay Y Adjustments: ").comment("Fine adjust the Y position for the overlay").defineInRange("y_adjustment", 10, Integer.MIN_VALUE, Integer.MAX_VALUE);
 
             SPEC = builder.build();
         }
